b449ab175a8a80649accd4b575459c99968bf4c6
thimote <thimoteeharerimana@gmail.com>
2019-03-29 10:20:29 +0200

    initial commit: basic java programming

--- encryption.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string = 'f12345';
-$string_hash = md5($string);
-echo $string_hash;
-
- ?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require 'core.inc.php';
-session_destroy();
-header('location: '.$http_referer);
- ?>
